4e520579384abfb66d489f488119c51817613402
Natalia Tatarinteva <natalia.tatarinteva@app-squared.com>
2020-07-09 13:48:30 +0200

    Create reusable custom alert

---
+++ GHFollowers/UIViewController+Ext.swift
@@ -0,0 +1,22 @@
+//
+//  UIViewController+Ext.swift
+//  GHFollowers
+//
+//  Created by Natalia Tatarinteva on 09.07.20.
+//  Copyright © 2020 com.app-squared. All rights reserved.
+//
+
+import UIKit
+
+extension UIViewController {
+    
+    func presentGFAlertOnMainThread(title: String, message: String, buttonTitle: String) {
+        DispatchQueue.main.async {
+            let alertVC = GFAlertViewController(title: title, message: message, buttonTitle: buttonTitle)
+            alertVC.modalPresentationStyle = .overFullScreen
+            alertVC.modalTransitionStyle = .crossDissolve
+            self.present(alertVC, animated: true)
+        }
+        
+    }
+}

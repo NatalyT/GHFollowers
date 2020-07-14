05347eebbdc33d2f4db20975a8af53d7f08c6088
Natalia Tatarinteva <natalia.tatarinteva@app-squared.com>
2020-07-14 17:01:59 +0200

    Add data models by planning network calls

---
+++ GHFollowers/Model/Follower.swift
@@ -0,0 +1,14 @@
+//
+//  Follower.swift
+//  GHFollowers
+//
+//  Created by Natalia Tatarinteva on 14.07.20.
+//  Copyright © 2020 com.app-squared. All rights reserved.
+//
+
+import Foundation
+
+struct Follower: Codable {
+    var login: String
+    var avatarUrl: String
+}

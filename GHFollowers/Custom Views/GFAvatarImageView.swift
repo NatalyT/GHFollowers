2ee19e1285dadf2b1a0b06f5cba5843db2472513
Natalia Tatarinteva <natalia.tatarinteva@app-squared.com>
2020-07-27 12:54:38 +0200

    Add custom Follower UICollectionView Cell

---
+++ GHFollowers/Custom Views/GFAvatarImageView.swift
@@ -0,0 +1,31 @@
+//
+//  GFAvatarImageView.swift
+//  GHFollowers
+//
+//  Created by Natalia Tatarinteva on 27.07.20.
+//  Copyright © 2020 com.app-squared. All rights reserved.
+//
+
+import UIKit
+
+class GFAvatarImageView: UIImageView {
+    
+    let placeholderImage = UIImage(named: "avatar-placeholder")!
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        configure()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    private func configure() {
+        layer.cornerRadius = 10
+        clipsToBounds = true
+        image = placeholderImage
+        translatesAutoresizingMaskIntoConstraints = false
+    }
+    
+}

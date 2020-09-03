9c39cd05bcd25e7e8808d43dff510a306da4a66a
Natalia Tatarinteva <natalia.tatarinteva@app-squared.com>
2020-09-03 11:35:10 +0200

    Child View Controller - UserInfoHeaderVC

---
+++ GHFollowers/Utilities/Constants.swift
@@ -0,0 +1,13 @@
+//
+//  Constants.swift
+//  GHFollowers
+//
+//  Created by Natalia Tatarinteva on 03.09.20.
+//  Copyright © 2020 com.app-squared. All rights reserved.
+//
+
+import Foundation
+
+enum SFSymbols {
+    static let location = "mappin.and.ellipse"
+}

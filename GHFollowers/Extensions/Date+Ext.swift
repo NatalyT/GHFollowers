2a1d6fd15ec75f38d66e6733797446406443a64b
Natalia Tatarinteva <natalia.tatarinteva@app-squared.com>
2020-09-11 22:15:33 +0200

    Date Conversions & Date Label

---
+++ GHFollowers/Extensions/Date+Ext.swift
@@ -0,0 +1,18 @@
+//
+//  Date+Ext.swift
+//  GHFollowers
+//
+//  Created by Natalia Tatarinteva on 11.09.20.
+//  Copyright © 2020 com.app-squared. All rights reserved.
+//
+
+import Foundation
+
+extension Date {
+    
+    func convertToMonthYearFormat() -> String {
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "MMM yyyy"
+        return dateFormatter.string(from: self)
+    }
+}

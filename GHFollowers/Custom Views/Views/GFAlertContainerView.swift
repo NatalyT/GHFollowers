030c6e58facf49792971cbe08d8903591e39b8f2
Natalia Tatarinteva <natalia.tatarinteva@app-squared.com>
2020-09-28 09:42:41 +0200

    GFAlertVC, Convenience Init, DateDecoding, Networking Refactor

--- GHFollowers/Custom Views/Views/GFAlertContainerView.swift
+++ GHFollowers/Custom Views/Views/GFAlertContainerView.swift
@@ -1,18 +1,15 @@
 //
-//  GFAvatarImageView.swift
+//  GFAlertContainerView.swift
 //  GHFollowers
 //
-//  Created by Natalia Tatarinteva on 27.07.20.
+//  Created by Natalia Tatarinteva on 25.09.20.
 //  Copyright © 2020 com.app-squared. All rights reserved.
 //
 
 import UIKit
 
-class GFAvatarImageView: UIImageView {
-    
-    let cache = NetworkManager.shared.cache
-    let placeholderImage = UIImage(named: "avatar-placeholder")!
-    
+class GFAlertContainerView: UIView {
+
     override init(frame: CGRect) {
         super.init(frame: frame)
         configure()
@@ -23,9 +20,10 @@ class GFAvatarImageView: UIImageView {
     }
     
     private func configure() {
-        layer.cornerRadius = 10
-        clipsToBounds = true
-        image = placeholderImage
+        backgroundColor = .systemBackground
+        layer.cornerRadius = 16
+        layer.borderWidth = 2
+        layer.borderColor = UIColor.white.cgColor
         translatesAutoresizingMaskIntoConstraints = false
     }
 }
--- GHFollowers/Screens/UserInfoViewController.swift
+++ GHFollowers/Screens/UserInfoViewController.swift
@@ -13,7 +13,7 @@ protocol UserInfoVCDelegate: class {
     func didTapGetFollowers(for user: User)
 }
 
-class UserInfoViewController: UIViewController {
+class UserInfoViewController: GFDataLoadingViewController {
     
     let headerView = UIView()
     let itemViewOne = UIView()
@@ -61,7 +61,7 @@ class UserInfoViewController: UIViewController {
         self.add(childVC: repoItemVC, to: self.itemViewOne)
         self.add(childVC: followerItemVC, to: self.itemViewTwo)
         self.add(childVC: GFUserInfoHeaderViewController(user: user), to: self.headerView)
-        self.dateLabel.text = "GitHub since \(user.createdAt.convertToDisplayFormat())"
+        self.dateLabel.text = "GitHub since \(user.createdAt.convertToMonthYearFormat())"
     }
     
     func layoutUI() {
